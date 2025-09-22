5f50981caa9a976884f4f2df9909add4581ae1e4
Ankit Kumar <97714826+ankit-kumar-developer-122@users.noreply.github.com>
2025-09-22 08:23:26 +0000

    Functions

---
+++ Kotlin Basics/three.kt
@@ -0,0 +1,69 @@
+// Parameters & Return Types
+fun printName() {
+    print("Adam")
+}
+
+fun printName(person: Person) {
+    print(person.name)
+}
+
+fun getGreeting(person: Person): String {
+    return "Hello, ${person.name}"
+}
+
+fun getGreeting(person: Person): String = "Hello, ${person.name}"
+fun getGreeting(person: Person) = "Hello, ${person.name}"
+
+
+
+// 2. Default Parameters
+fun getGreeting(person: Person, intro: String = "Hello,"): String {
+    return "$intro ${person.name}"
+}
+
+// Returns "Hello, Adam"
+val hello = getGreeting(Person("Adam"))
+
+// Returns "Welcome, Adam"
+val welcome = getGreeting(Person("Adam"), "Welcome,")
+
+
+// 3. Higher Order Functions
+fun callbackIfTrue(condition: Boolean, callback: () -> Unit) {
+    if (condition) {
+        callback()
+    }
+}
+
+callbackIfTrue(someBoolean) {
+    print("Condition was true")
+}
+
+// 4. Named Parameters
+class Person(val name: String = "", age: Int = 0)
+
+// All valid
+val person = Person()
+val person = Person("Adam", 100)
+val person = Person(name = "Adam", age = 100)
+val person = Person(age = 100)
+val person = Person(age = 100, name = "Adam")
+
+// 5. Static Functions
+class Fragment(val args: Bundle) {
+    companion object {
+        fun newInstance(args: Bundle): Fragment {
+            return Fragment(args)
+        }
+    }
+}
+
+val fragment = Fragment.newInstance(args)
+Companion Objects
+
+// 6. Extension Functions
+fun Int.timesTwo(): Int {
+    return this * 2
+}
+
+val four = 2.timesTwo()
